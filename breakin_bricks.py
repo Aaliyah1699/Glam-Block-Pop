0526e21ffef8d25ddd0d0eb42217982024036546
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-08-11 12:44:46 -0500

    Place bricks on screen

--- breakin_bricks.py
+++ breakin_bricks.py
@@ -19,6 +19,14 @@ brick = pygame.image.load('./images/brick.png')
 brick = brick.convert_alpha()
 brick_rect = brick.get_rect()
 bricks = []
+brick_rows = 5
+brick_cols = 3
+#  Calculate bricks
+for y in range(brick_rows):
+    brickY = y * brick_rect[3]
+    for x in range(brick_cols):
+        brickX = x * brick_rect[2]
+        bricks.append((brickX, brickY))
 
 clock = pygame.time.Clock()
 game_over = False
@@ -26,9 +34,14 @@ game_over = False
 while not game_over:
     dt = clock.tick(50)
     screen.fill((0, 0, 0))
+    #  Place bricks on screen
+    for b in bricks:
+        screen.blit(brick, b)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+    pygame.display.update()
 
 pygame.quit()
